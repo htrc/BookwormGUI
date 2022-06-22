2ffa1b15b7f4de4d5c1c234a97e54150988179e0
Deren Kudeki <dkudeki@illinois.edu>
2022-06-21 19:33:31 -0500

    Turn this into string

--- app/bookworm.gui.js
+++ app/bookworm.gui.js
@@ -1006,7 +1006,7 @@
     };
     handleLegendToggle = function(event) {
       var visibility = this.visible ? 'visible' : 'hidden';
-      alert(this)
+      console.log(JSON.stringify(this))
     };
     page = 1;
     $("#books").on("click", "ul.pagination a", function(event) {
